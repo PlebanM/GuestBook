7dbc9ae1a152a4b9af383b24abd892d76ef19999
plebanm <pleban.marcin@gmail.com>
2019-05-12 18:07:54 +0200

    Delete unnecessary pages, repair "resend data" when reload page.

---
+++ src/main/java/AddMessage.java
@@ -0,0 +1,49 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import model.BookEntry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddMessage implements HttpHandler {
+
+    private List<BookEntry> bookEntries;
+
+    public AddMessage() {
+        this.bookEntries = new ArrayList<>();
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        bookEntries.add(getDataFromForm(httpExchange));
+        redirectToResourcePage(httpExchange);
+
+    }
+
+    public List<BookEntry> getBookEntries() {
+        return bookEntries;
+    }
+
+    private void redirectToResourcePage(HttpExchange httpExchange) throws IOException {
+        httpExchange.getResponseHeaders().set("Location", "/guestbook");
+        httpExchange.sendResponseHeaders(303,0);
+
+
+    }
+
+    private BookEntry getDataFromForm(HttpExchange exchange) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
+        String userCredentials = br.readLine();
+        String[] entries = userCredentials.split("&");
+        String message = URLDecoder.decode(entries[0].split("=")[1].trim(), StandardCharsets.UTF_8.toString());
+        String author = URLDecoder.decode(entries[1].split("=")[1].trim(), StandardCharsets.UTF_8.toString());
+        return new BookEntry(message, author);
+
+    }
+}
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,15 @@
 import com.sun.net.httpserver.HttpServer;
 
-import javax.xml.ws.spi.http.HttpHandler;
 import java.net.InetSocketAddress;
 
 public class App {
 
     public static void main(String[] args) throws Exception{
-
+        AddMessage addMessage = new AddMessage();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
 
-//        server.createContext("/guestBook", new GuestBook());
+        server.createContext("/guestbook", new NewGuestBook(addMessage));
         server.createContext("/static", new Static());
-        server.createContext("/form", new Form());
-        server.createContext("/temp", new GuestBook());
-        server.createContext("/new", new NewGuestBook());
-
         server.setExecutor(null);
 
         server.start();
